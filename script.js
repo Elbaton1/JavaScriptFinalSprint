59cd78620afa259f49e876da8c513ba374951d45
Elbaton1 <164584272+Elbaton1@users.noreply.github.com>
2024-04-14 15:29:19 -0230

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// Test if script is linked correctly
 console.log("Script linked successfully!");
-
-// Step 3: Reading the JSON file
 fetch("superheroes.json")
   .then((response) => response.json())
   .then((data) => {
     console.log(data); // Logging the data to console for verification
 
-    //Iterating over JSON data and logging names
+    
     data.forEach((item) => {
       console.log(item.name); // Displaying the name of each hero
     });
